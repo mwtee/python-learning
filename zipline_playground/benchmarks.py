405389da4301c5203c0616d6723db43fd6155924
Ming Wei Tee <mingwei.tee@gmail.com>
2020-11-04 23:18:30 +1100

    Introduce backtesting using zipline ingested data and zipline backtester

--- zipline_playground/benchmarks.py
+++ zipline_playground/benchmarks.py
@@ -50,3 +50,67 @@ def get_benchmark_returns_from_file(filelike):
                          "2020-01-03 00:00:00+00:00,-0.02\n")
 
     return df['return'].sort_index()
+
+from zipline import run_algorithm
+from zipline.api import order_target_percent, symbol
+from datetime import datetime
+import pytz
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def initialize(context):
+    context.stock = symbol("AAPL")
+    context.index_average_window = 100
+    
+def handle_data(context, data):
+    # Request history for the stock
+    equities_hist = data.history(
+        context.stock,
+        "close",
+        context.index_average_window, 
+        "1d")
+    
+    if equities_hist[-1] > equities_hist.mean():
+        stock_weight = 1.0
+    else:
+        stock_weight = 0.0
+        
+    order_target_percent(context.stock, stock_weight)
+    
+def analyze(context, perf):
+    fig = plt.figure(figsize=(12, 8))
+    
+    ax = fig.add_subplot(311)
+    ax.set_title("Strategy Results")
+    ax.semilogy(perf["portfolio_value"], linestyle="-", label="Equity curve", linewidth=3.0)
+    ax.legend()
+    ax.grid(False)
+    
+    ax = fig.add_subplot(312)
+    ax.plot(
+        perf["gross_leverage"],
+        label="Exposure",
+        linestyle="-",
+        linewidth=1.0)
+    ax.legend()
+    ax.grid(True)
+    
+    ax = fig.add_subplot(313)
+    ax.plot(perf["returns"], label="Returns", linestyle="-", linewidth=1.0)
+    ax.legend()
+    ax.grid(True)
+    
+    
+start_date = pd.Timestamp('2017-1-1', tz='utc')
+end_date = pd.Timestamp('2018-03-31', tz='utc')
+type(start_date)
+
+results = run_algorithm(
+    start=start_date,
+    end=end_date,
+    initialize=initialize, 
+    analyze=analyze,
+    handle_data=handle_data,
+    capital_base=100000,
+    data_frequency = "daily",
+    bundle="quandl")
