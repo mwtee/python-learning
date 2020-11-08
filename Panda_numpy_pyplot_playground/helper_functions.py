12605c9e56207ed272d8e31d2a11e604e38670f8
Huan Nguyen <huan.nguyentuong@gmail.com>
2020-11-08 15:13:28 +1100

    Reformat the code to confirm to Pycharm's default code style

--- Panda_numpy_pyplot_playground/helper_functions.py
+++ Panda_numpy_pyplot_playground/helper_functions.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def read_csv(path, index_col, use_cols):
     """Read excel file base on the file path and index col and returns the data frame."""
-    return pd.read_csv(path, index_col = index_col, parse_dates = [index_col], usecols=use_cols)
+    return pd.read_csv(path, index_col=index_col, parse_dates=[index_col], usecols=use_cols)
+
 
 def calculate_corr(series1, series2, window):
     """
@@ -21,6 +23,7 @@ def calculate_corr(series1, series2, window):
     ret1 = series1.pct_change()
     ret2 = series2.pct_change()
     return ret1.rolling(window).corr(ret2)
+
 
 def plot_graph(data, legends, xLabel="", yLabel=""):
     """Plot the a graph base on the params."""
--- zipline_playground/backtesting.py
+++ zipline_playground/backtesting.py
@@ -5,18 +5,20 @@ import pytz
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def initialize(context):
     context.stock = symbol("AAPL")
     context.index_average_window = 100
-    
+
+
 def handle_data(context, data):
     # Request history for the stock
     equities_hist = data.history(
         context.stock,
         "close",
-        context.index_average_window, 
+        context.index_average_window,
         "1d")
-    
+
     # Compute averages
     short_mavg = data.history(context.stock, 'price', bar_count=20, frequency="1d").mean()
     long_mavg = data.history(context.stock, 'price', bar_count=50, frequency="1d").mean()
@@ -26,13 +28,14 @@ def handle_data(context, data):
         order_target(context.stock, 100)
     elif short_mavg < long_mavg:
         order_target(context.stock, 0)
-        
+
     # Save values for later inspection
     record(AAPL=data.current(context.stock, 'price'),
-            short_mavg=short_mavg,
-            long_mavg=long_mavg)
-        
-def analyze(context, perf):        
+           short_mavg=short_mavg,
+           long_mavg=long_mavg)
+
+
+def analyze(context, perf):
     fig = plt.figure(figsize=(12, 8))
     ax1 = fig.add_subplot(211)
     perf.portfolio_value.plot(ax=ax1)
@@ -47,21 +50,21 @@ def analyze(context, perf):
     sells = perf_trans.loc[
         [t[0]['amount'] < 0 for t in perf_trans.transactions]]
     ax2.plot(buys.index, perf.short_mavg.loc[buys.index],
-              '^', markersize=10, color='m')
+             '^', markersize=10, color='m')
     ax2.plot(sells.index, perf.short_mavg.loc[sells.index],
-              'v', markersize=10, color='k')
+             'v', markersize=10, color='k')
     ax2.set_ylabel('price in $')
     plt.legend(loc=0)
     plt.show()
-    
+
     # fig = plt.figure(figsize=(12, 8))
-    
+
     # ax = fig.add_subplot(311)
     # ax.set_title("Strategy Results")
     # ax.semilogy(perf["portfolio_value"], linestyle="-", label="Equity curve", linewidth=3.0)
     # ax.legend()
     # ax.grid(False)
-    
+
     # ax = fig.add_subplot(312)
     # ax.plot(
     #     perf["gross_leverage"],
@@ -70,12 +73,13 @@ def analyze(context, perf):
     #     linewidth=1.0)
     # ax.legend()
     # ax.grid(True)
-    
+
     # ax = fig.add_subplot(313)
     # ax.plot(perf["returns"], label="Returns", linestyle="-", linewidth=1.0)
     # ax.legend()
     # ax.grid(True)
-    
+
+
 start_date = pd.Timestamp('2017-09-1', tz='utc')
 end_date = pd.Timestamp('2018-03-31', tz='utc')
 type(start_date)
@@ -83,9 +87,9 @@ type(start_date)
 results = run_algorithm(
     start=start_date,
     end=end_date,
-    initialize=initialize, 
+    initialize=initialize,
     analyze=analyze,
     handle_data=handle_data,
     capital_base=100000,
-    data_frequency = "daily",
+    data_frequency="daily",
     bundle="quandl")
